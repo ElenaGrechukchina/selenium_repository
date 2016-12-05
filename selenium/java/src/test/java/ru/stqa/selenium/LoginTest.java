9509f81ab287ca91f8f4403b4689d3abc62d0955
ElenaGrechukhina <grechukhina.elena@gmail.com>
2016-12-05 12:04:22 +0300

    Сценарий логина, второе ДЗ

--- selenium/java/src/test/java/ru/stqa/selenium/LoginTest.java
+++ selenium/java/src/test/java/ru/stqa/selenium/LoginTest.java
@@ -9,10 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
-
-public class MyFirstTest {
-
+public class LoginTest {
     private WebDriver driver;
     private WebDriverWait wait;
 
@@ -23,14 +20,11 @@ public class MyFirstTest {
     }
 
     @Test
-    public void myFirstTest(){
-       // driver.get("http://www.google.com/");
-       // driver.findElement(By.name("q")).sendKeys("webdriver");
-       // driver.findElement(By.name("btnG")).click();
-       // wait.until(titleIs("webdriver - Поиск в Google"));
-
-        driver.get("localhost/litecart/admin/");
-
+    public void loginTest(){
+        driver.get("http://localhost/litecart/admin/login.php");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
     }
 
     @After
@@ -38,5 +32,4 @@ public class MyFirstTest {
         driver.quit();
         driver = null;
     }
-
 }
